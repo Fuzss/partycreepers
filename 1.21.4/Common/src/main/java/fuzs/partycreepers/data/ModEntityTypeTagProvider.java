a54287700fd2186f9caf82d7d3206d517b0b0127
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-02-11 11:55:10 +0100

    prepare 1.21.4 port

---
+++ 1.21.4/Common/src/main/java/fuzs/partycreepers/data/ModEntityTypeTagProvider.java
@@ -0,0 +1,22 @@
+package fuzs.partycreepers.data;
+
+import fuzs.partycreepers.init.ModRegistry;
+import fuzs.puzzleslib.api.data.v2.core.DataProviderContext;
+import fuzs.puzzleslib.api.data.v2.tags.AbstractTagProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.entity.EntityType;
+
+public class ModEntityTypeTagProvider extends AbstractTagProvider<EntityType<?>> {
+
+    public ModEntityTypeTagProvider(DataProviderContext context) {
+        super(Registries.ENTITY_TYPE, context);
+    }
+
+    @Override
+    public void addTags(HolderLookup.Provider provider) {
+        this.add(ModRegistry.EXPLOSIVE_CREEPERS_ENTITY_TYPE_TAG)
+                .add(EntityType.CREEPER)
+                .addOptionalTag("creeperoverhaul:creepers");
+    }
+}
