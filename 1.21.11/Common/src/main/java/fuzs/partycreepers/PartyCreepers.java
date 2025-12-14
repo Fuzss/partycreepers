8d9fe88834e4c833cd7df4aed0620007eac9b085
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-12-14 11:35:42 +0100

    release v21.11.0

--- 1.21.11/Common/src/main/java/fuzs/partycreepers/PartyCreepers.java
+++ 1.21.11/Common/src/main/java/fuzs/partycreepers/PartyCreepers.java
@@ -5,9 +5,9 @@ import fuzs.partycreepers.handler.CreeperConfettiHandler;
 import fuzs.partycreepers.init.ModRegistry;
 import fuzs.puzzleslib.api.config.v3.ConfigHolder;
 import fuzs.puzzleslib.api.core.v1.ModConstructor;
-import fuzs.puzzleslib.api.core.v1.utility.ResourceLocationHelper;
+import net.minecraft.resources.Identifier;
 import fuzs.puzzleslib.api.event.v1.level.ExplosionEvents;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,7 +29,7 @@ public class PartyCreepers implements ModConstructor {
         ExplosionEvents.DETONATE.register(CreeperConfettiHandler::onExplosionDetonate);
     }
 
-    public static ResourceLocation id(String path) {
-        return ResourceLocationHelper.fromNamespaceAndPath(MOD_ID, path);
+    public static Identifier id(String path) {
+        return Identifier.fromNamespaceAndPath(MOD_ID, path);
     }
 }
